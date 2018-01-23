b707d548acdc59407166c75345c1fe606785a108
Zushisan <74838@supinfo.com>
2018-01-23 18:16:48 +0000

    url index url show and header set up

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-// express server.js
+/* express_server.js*/
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -17,9 +17,15 @@ app.get("/urls", (req,res) =>{
  });
 });
 
+// app.get("/urls/:id", (req,res) =>{
 app.get("/urls/b2xVn2", (req,res) =>{
 
-})
+ // let templateVar = {shortURL: req.params.id};
+ // why does above format not work, but below does?
+ res.render("urls_show", {shortURL: req.params.id});
+ // console.log(req.params.id);
+
+});
 
 app.listen(8080);
 console.log('8080 is the magic port');
