4a182f37c444086984969c2f40c2559802083b3a
Zushisan <74838@supinfo.com>
2018-01-24 17:34:46 +0000

    revert merge

--- express_server.js
+++ express_server.js
@@ -26,12 +26,14 @@ for (var i = 0; i < 6; i++){
   text += possible.charAt(Math.floor(Math.random() * possible.length));
 }
 
+
 var object = {};
 object[text] = longURL;
 urlDatabase.push(object);
 
 return text;
 }
+var baseURL = process.env.ROOT_URL || 'http://localhost:8080/';
 
 // redirect
 app.get("/", (req,res) =>{
@@ -55,6 +57,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVar = {
     shortURL: req.params.id,
+    baseURL: baseURL,
    username: req.cookies["username"]
  };
   res.render('urls_show', templateVar);
