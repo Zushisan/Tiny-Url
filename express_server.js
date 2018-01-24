bfa385dbf490cb469b0e16f5c66346e085b7df14
Zushisan <74838@supinfo.com>
2018-01-24 20:12:29 +0000

    login changes v1

--- express_server.js
+++ express_server.js
@@ -10,14 +10,26 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var urlDatabase = [
+const urlDatabase = [
   //{ shortURL: longURL},
   { "b2xVn2": "http://www.google.ca" },
   { "9sm5xK": "http://www.lighthouse.ca" }
 ];
 
+const userDatabase = {
+  "Jonathan": {
+    userID: "Jonathan",
+    email: "Jonathan@me.com",
+    password: "password"
+  },
+  "test": {
+    userID: "test",
+    email: "test@me.com",
+    password: "1234"}
+};
 
-//Random string generator
+
+//Random url generator
 function generateRandomString(longURL) {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -33,6 +45,22 @@ function generateRandomString(longURL) {
   return text;
 }
 
+//Random user ID generator
+function generateRandomID(userInfo) {
+  var randomID = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 4; i++){
+    randomID += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  var object = userInfo;
+  object.userID = randomID;
+  userDatabase[randomID] = object;
+
+  return randomID
+}
+
 var baseURL = process.env.ROOT_URL || 'http://localhost:8080/';
 
 // redirect
@@ -42,15 +70,21 @@ app.get("/", (req,res) =>{
 
 //home
 app.get("/urls", (req,res) =>{
-  res.render('urls_index', {
-   key: urlDatabase,
-   username: req.cookies["username"]
-  });
+  let templateVar = {
+    key: urlDatabase,
+    userDatabaseKey: userDatabase,
+    cookie: req.cookies["userID"]
+  };
+  res.render('urls_index', templateVar);
 });
 
 //form page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", {username: req.cookies["username"]});
+  let templateVar = {
+    userDatabaseKey: userDatabase,
+    cookie: req.cookies["userID"]
+  };
+  res.render("urls_new", templateVar);
 });
 
 //gives short url
@@ -58,7 +92,8 @@ app.get("/urls/:id", (req, res) => {
   let templateVar = {
     shortURL: req.params.id,
     baseURL: baseURL,
-   username: req.cookies["username"]
+    userDatabaseKey: userDatabase,
+    cookie: req.cookies["userID"]
  };
   res.render('urls_show', templateVar);
 });
@@ -73,17 +108,52 @@ app.get("/u/:shortURL", (req, res) => {
        longURL = urlDatabase[i][shortURL];
      }
    }
-   console.log("long URL: ", longURL)
+   console.log("long URL: ", longURL);
  res.redirect(longURL);
 });
 
+//Registration page
+app.get("/register", (req, res) => {
+  let templateVar = {
+    userDatabaseKey: userDatabase,
+    cookie: req.cookies["userID"]
+  };
+  res.render("register", templateVar);
+});
+
+// login page
+app.get("/login", (req, res) => {
+  let templateVar = {
+    userDatabaseKey: userDatabase,
+    cookie: req.cookies["userID"]
+  };
+  res.render("login", templateVar);
+});
+
+// Registration Form
+app.post('/register', (req, res) => {
+  let randomID = generateRandomID(req.body);
+  // if (req.body.email === "" || req.body.password === ""){
+  //   res.status(400)
+  //   res.send("fefe");
+  // }
+  // for (let i = 0; i < urlDatabase; i++){
+  //   if (req.body.email === urlDatabse[i][email]){
+  //     res.status(400);
+  //     // .send("Email already in use")
+  //   }
+  // }
+
+  // else
+  res.cookie("userID", randomID);
+  res.redirect('/urls');
+  console.log(userDatabase);
+})
 
 //form
 app.post("/urls", (req, res) => {
-let shortURL = generateRandomString(req.body.longURL);
-
-// res.send("Ok");
-res.redirect("/urls");
+  let shortURL = generateRandomString(req.body.longURL);
+  res.redirect("/urls");
 });
 
 // Delete button
@@ -108,24 +178,35 @@ app.post("/urls/:id", (req, res) => {
        urlDatabase[i][updateKey] = updateValue;
      }
    }
-  res.redirect("/urls");
+res.redirect("/urls");
 });
 
 // Login Form
 app.post("/login", (req, res) => {
-  let user = req.body.username;
-  res.cookie("username", user);
+  let userEmail = req.body.email;
+  let userPass = req.body.password;
+
+  for (let key in userDatabase){ // go throught each key in database
+    if(key.email === userEmail){ // check for email
+      if(key.password === userPass){ // check for password
+        res.cookie("userID", key); // if everything is good, set cookie
+      }
+    }
+  }
+
+    // if no password re enter passwrod WRONG
+
+  // else go register
   res.redirect("/urls");
 });
 
 // Logout
 app.post("/logout", (req, res) => {
-  console.log(req.body);
-  let user = req.body.username;
-  res.clearCookie("username", user);
+  console.log(req.cookies);
+  let user = req.cookies;
+  res.clearCookie("userID", user.userID);
   res.redirect("/urls");
 });
-
 
 
 app.listen(8080);
